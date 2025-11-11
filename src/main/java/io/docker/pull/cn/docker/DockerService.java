670996cb34b0edeafb53f00063b3051f04ec93fa
jiangtao <jiangtao.cctv@qq.com>
2025-11-11 17:19:01 +0800

    init

--- src/main/java/io/docker/pull/cn/docker/DockerService.java
+++ src/main/java/io/docker/pull/cn/docker/DockerService.java
@@ -1,35 +1,77 @@
 package io.docker.pull.cn.docker;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.system.SystemUtil;
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.async.ResultCallback;
 import com.github.dockerjava.api.command.InspectImageResponse;
 import com.github.dockerjava.api.model.ResponseItem;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientConfig;
+import com.github.dockerjava.core.DockerClientImpl;
+import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
+import com.github.dockerjava.transport.DockerHttpClient;
 import io.docker.pull.cn.config.SysProp;
+import jakarta.annotation.PostConstruct;
 import jakarta.annotation.Resource;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
 @Slf4j
 @Component
-@AllArgsConstructor
 public class DockerService {
 
+    private DockerClient cli;
 
+    private DockerClient cliRemote;
 
     @Resource
-    DockerClient cli;
+    private SysProp sysProp;
 
-    @Resource
-    SysProp sysProp;
+    @PostConstruct
+    public void init() {
+        this.cli = this.getCli(false);
+        this.cliRemote = this.getCli(true);
+    }
+
+    private DockerClient getCli(boolean useRemote) {
+        SysProp.Registry registry = sysProp.getRegistry();
+
+
+        boolean windows = SystemUtil.getOsInfo().isWindows();
+        String dockerHost = windows ? "tcp://localhost:2375" : "unix:///var/run/docker.sock";
+        log.info("docker主机路径：{}", dockerHost);
+
+        DefaultDockerClientConfig.Builder builder = DefaultDockerClientConfig.createDefaultConfigBuilder()
+                .withDockerHost(dockerHost);
+
+        if (useRemote) {
+            if (StrUtil.isAllNotEmpty(registry.getUser(), registry.getPwd())) {
+                builder.withRegistryUrl(registry.getUrl())
+                        .withRegistryUsername(registry.getUser())
+                        .withRegistryPassword(registry.getPwd());
+            }
+        }
+
+
+        DockerClientConfig config = builder.build();
+
+        DockerHttpClient httpClient = new ApacheDockerHttpClient.Builder()
+                .dockerHost(config.getDockerHost())
+                .sslConfig(config.getSSLConfig())
+                .build();
+
+
+        return DockerClientImpl.getInstance(config, httpClient);
+    }
 
 
     public void pullRemoteAndChangeTag(String image) throws InterruptedException {
         String targetImage = getChangedTargetImageName(image);
         log.info("转换镜像地址 {}", targetImage);
 
-      this.pull(targetImage);
+        this.pull(targetImage);
 
         this.changeTag(targetImage, image);
     }
@@ -48,7 +90,7 @@ public class DockerService {
     }
 
     private String getChangedTargetImageName(String image) {
-        String repository =  sysProp.getTargetRepository();
+        String repository = sysProp.getTargetRepository();
         String targetImage = repository + ":" + image.replace("/", "_").replace(":", "_");
         return targetImage;
     }
@@ -65,8 +107,8 @@ public class DockerService {
 
 
     public void push(String image) throws InterruptedException {
-        log.info("开始推送镜像:{}",image);
-        cli.pushImageCmd(image).exec(getCallback()).awaitCompletion();
+        log.info("开始推送镜像:{}", image);
+        cliRemote.pushImageCmd(image).exec(getCallback()).awaitCompletion();
     }
 
     private static <T extends ResponseItem> ResultCallback.Adapter<T> getCallback() {
@@ -80,8 +122,8 @@ public class DockerService {
                     log.info("进度 {}", progress);
                 } else if (status != null) {
                     log.info("状态 {}", status);
-                }else if(item.getErrorDetail() != null && item.getErrorDetail().getMessage() != null) {
-                    log.info("错误 {}",item.getErrorDetail().getMessage());
+                } else if (item.getErrorDetail() != null && item.getErrorDetail().getMessage() != null) {
+                    log.info("错误 {}", item.getErrorDetail().getMessage());
                 }
                 super.onNext(item);
             }
